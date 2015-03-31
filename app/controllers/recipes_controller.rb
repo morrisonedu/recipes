a3380214ef2ad84d1cab939740e5d60d5a976b9f
jeff m <info@morrisonedu.com>
2015-03-31 03:59:15 +0000

    added even more

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,8 +5,27 @@ class RecipesController < ApplicationController
   
   def show
     @recipe=Recipe.find(params[:id])
-    
-    
   end
+  
+  def new
+   @recipe = Recipe.new 
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "Thanks for creating a recipe!"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :sumary, :description)
+    end
   
 end

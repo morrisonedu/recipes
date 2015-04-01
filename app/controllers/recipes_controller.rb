2184a085f204b7ec39eb3ed6e22b7df7988646a0
jeff m <info@morrisonedu.com>
2015-04-01 16:26:39 +0000

    added more

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,6 +23,22 @@ class RecipesController < ApplicationController
     end
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update 
+    @recipe = Recipe.find(params[:id])
+    
+    if @recipe.update(recipe_params)
+      flash[:success] = "You updated the recipe successfully."
+      redirect_to recipes_path(@recipe)
+    else
+      render :edit
+    end
+    
+  end
+  
   private
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description)
